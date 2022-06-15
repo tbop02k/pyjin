8d07324e3dfac69c4903e2402b0ae4aa4c9a803c
tbop02 <tbop02@gmail.com>
2022-06-15 02:51:50 +0000

    time series plot added

--- pyjin_pandas.py
+++ pyjin_pandas.py
@@ -90,3 +90,88 @@ def make_window_stats_column(
     rolling_std_temp = temp.std().shift(lag).rename(f'{col}_rolling_std_{window_size}')
 
     return rolling_mean_temp, rolling_std_temp
+
+def reduce_mem_usage(props):
+    start_mem_usg = props.memory_usage().sum() / 1024**2
+    print("Memory usage of properties dataframe is :",start_mem_usg," MB")
+    NAlist = [] # Keeps track of columns that have missing values filled in.
+    for col in props.columns:
+        if props[col].dtype != object:  # Exclude strings
+
+            # Print current column type
+            print("******************************")
+            print("Column: ",col)
+            print("dtype before: ",props[col].dtype)
+
+            # make variables for Int, max and min
+            IsInt = False
+            mx = props[col].max()
+            mn = props[col].min()
+
+            # Integer does not support NA, therefore, NA needs to be filled
+            if not np.isfinite(props[col]).all():
+                NAlist.append(col)
+                props[col].fillna(mn-1,inplace=True)
+
+                # test if column can be converted to an integer
+            asint = props[col].fillna(0).astype(np.int64)
+            result = (props[col] - asint)
+            result = result.sum()
+            if result > -0.01 and result < 0.01:
+                IsInt = True
+
+
+            # Make Integer/unsigned Integer datatypes
+            if IsInt:
+                if mn >= 0:
+                    if mx < 255:
+                        props[col] = props[col].astype(np.uint8)
+                    elif mx < 65535:
+                        props[col] = props[col].astype(np.uint16)
+                    elif mx < 4294967295:
+                        props[col] = props[col].astype(np.uint32)
+                    else:
+                        props[col] = props[col].astype(np.uint64)
+                else:
+                    if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
+                        props[col] = props[col].astype(np.int8)
+                    elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
+                        props[col] = props[col].astype(np.int16)
+                    elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
+                        props[col] = props[col].astype(np.int32)
+                    elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
+                        props[col] = props[col].astype(np.int64)
+
+                        # Make float datatypes 32 bit
+            else:
+                props[col] = props[col].astype(np.float32)
+
+            # Print new column type
+            print("dtype after: ",props[col].dtype)
+            print("******************************")
+
+    # Print final result
+    print("___MEMORY USAGE AFTER COMPLETION:___")
+    mem_usg = props.memory_usage().sum() / 1024**2
+    print("Memory usage is: ",mem_usg," MB")
+    print("This is ",100*mem_usg/start_mem_usg,"% of the initial size")
+    return props, NAlist
+
+
+def plot_timeseires(df):
+    '''
+    df should have datetime index    
+    '''
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import matplotlib.dates as md
+    sns.set(font_scale = 1.6)
+    
+    fig, ax = plt.subplots(figsize=(20,6))
+    g= sns.lineplot(data=df, marker='o')
+    g.set(ylim=(0, None))
+    ax.tick_params(axis='x', rotation=45)
+    ax.xaxis.set_major_locator(md.MonthLocator(interval = 1))
+    ax.set_xlabel('datetime')
+    ax.legend(fontsize=16)
+    ax.legend(fontsize=16)
